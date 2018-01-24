bcdd9167669448b7c967328d20d8dc74ac8b051c
Lunatic <2972512952@qq.com>
2018-01-24 16:29:37 +0800

    2018/1/24 asp.net Core 2.0 Web api 基础框架（1）——学习

---
+++ WebApp_Core/Controllers/MeterialController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_Core.Services;
+
+namespace WebApp_Core.Controllers
+{
+    [Route("api/product")]
+    public class MeterialController : Controller
+    {
+        [HttpGet("{productId}/materials")]
+        public IActionResult GetMeterials(int? productId)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == productId);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product.Materials);
+        }
+
+        [HttpGet("{productId}/materials/{id}")]
+        public IActionResult GetMeterial(int ?productId, int? id)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == productId);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            var material = product.Materials.SingleOrDefault(x => x.Id == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return Ok(material);
+        }
+    }
+}
---
+++ WebApp_Core/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_Core.Models;
+using WebApp_Core.Services;
+
+namespace WebApp_Core.Controllers
+{
+    [Route("api/product")]
+    public class ProductController : Controller
+    {
+        [HttpGet("all")]
+        public IActionResult GetProducts()
+        {
+            return Ok(ProductService.Current.Products);
+        }
+
+        [Route("{id}")]
+        public IActionResult GetProduct(int? id)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
