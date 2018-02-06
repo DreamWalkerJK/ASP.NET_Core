17b8af243fd339e8e6dd8c582b3d84ed9bc42164
Lunatic <2972512952@qq.com>
2018-02-06 10:52:52 +0800

    2018/2/6 官方WebApp Demo

---
+++ WepApp_Demo/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WepApp_Demo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
